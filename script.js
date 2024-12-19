83903ac5da6ec075d9828fdc82329d7d5c49532f
YourGiver <thehollowknight30@gmail.com>
2024-12-18 23:36:31 -0500

    Cumple de Jimena
    Jimena Ayo

--- script.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    // Verificar si el sobre no tiene la clase 'open'
-    if (!$('.envelope').hasClass('open')) {
-        // Añadir funcionalidad de clic
-        $('.envelope').click(function() {
-            $(this).removeClass('new').addClass('open');
-        });
-    }
-});
